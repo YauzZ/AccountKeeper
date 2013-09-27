5a61c0d91a79696853c514992f9f7d176b4cf649
Colin Yang Hong <colinyang@nmg.com.hk>
2013-09-27 17:35:00 +0800

    Initial Commit

---
+++ AccountKeeper/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AccountKeeper
+//
+//  Created by Colin Yang Hong on 13-9-27.
+//  Copyright (c) 2013年 Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
