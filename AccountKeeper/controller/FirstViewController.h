8fa1b0b97ebc81ed02c909728a2bea64badfce57
Colin Yang Hong <colinyang@nmg.com.hk>
2013-09-27 18:24:05 +0800

    添加主界面

---
+++ AccountKeeper/controller/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  AccountKeeper
+//
+//  Created by Colin Yang Hong on 13-9-27.
+//  Copyright (c) 2013年 Yang Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ AccountKeeper/controller/FirstViewController.m
@@ -0,0 +1,41 @@
+//
+//  FirstViewController.m
+//  AccountKeeper
+//
+//  Created by Colin Yang Hong on 13-9-27.
+//  Copyright (c) 2013年 Yang Hong. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *leftTableView;
+
+@property (weak, nonatomic) IBOutlet UITableView *rightTableView;
+@property (strong, nonatomic) IBOutletCollection(UITableViewCell) NSArray *tableViewCell;
+@end
+
+@implementation FirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
