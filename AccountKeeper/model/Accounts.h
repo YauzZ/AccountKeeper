8485ff90deecdedc567909a6155c8b3d1cd159ed
Colin Yang Hong <colinyang@nmg.com.hk>
2013-09-29 18:09:03 +0800

    修改Account数据模型

--- AccountKeeper/model/Accounts.h
+++ AccountKeeper/model/Accounts.h
@@ -2,7 +2,7 @@
 //  Accounts.h
 //  AccountKeeper
 //
-//  Created by Colin Yang Hong on 13-9-27.
+//  Created by Colin Yang Hong on 13-9-29.
 //  Copyright (c) 2013年 Yang Hong. All rights reserved.
 //
 
